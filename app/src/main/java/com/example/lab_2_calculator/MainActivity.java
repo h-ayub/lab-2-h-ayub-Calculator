9de5c147bf4ad490430240c65d35ece614c3458b
h-ayub <ayub.humza@gmail.com>
2021-09-21 11:46:50 -0500

    Version 1.0 of the Calculator

--- app/src/main/java/com/example/lab_2_calculator/MainActivity.java
+++ app/src/main/java/com/example/lab_2_calculator/MainActivity.java
@@ -2,9 +2,55 @@ package com.example.lab_2_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void add(View view) {
+        EditText firstTextField = (EditText) findViewById(R.id.firstTextNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.secondTextNumber);
+
+        int result = Integer.parseInt(firstTextField.getText().toString()) + Integer.parseInt(secondTextField.getText().toString());
+
+        goToActivity2("" + result);
+    }
+
+    public void subtract(View view) {
+        EditText firstTextField = (EditText) findViewById(R.id.firstTextNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.secondTextNumber);
+
+        int result = Integer.parseInt(firstTextField.getText().toString()) - Integer.parseInt(secondTextField.getText().toString());
+
+        goToActivity2("" + result);
+    }
+
+    public void multiply(View view) {
+        EditText firstTextField = (EditText) findViewById(R.id.firstTextNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.secondTextNumber);
+
+        int result = Integer.parseInt(firstTextField.getText().toString()) * Integer.parseInt(secondTextField.getText().toString());
+
+        goToActivity2("" + result);
+    }
+
+    public void divide(View view) {
+        EditText firstTextField = (EditText) findViewById(R.id.firstTextNumber);
+        EditText secondTextField = (EditText) findViewById(R.id.secondTextNumber);
+
+        int result = Integer.parseInt(firstTextField.getText().toString()) / Integer.parseInt(secondTextField.getText().toString());
+
+        goToActivity2("" + result);
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
